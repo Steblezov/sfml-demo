f44502823189aff050d6ce5eb26ccd0d8b8fd4fc
Alexandr <cteblezov@gmail.com>
2021-10-25 19:03:22 +0700

    trying to make a smooth movementе

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,74 @@
 #include<iostream>
 #include<SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
 
-using namespace std;
-using namespace sf;
+
+
+using Clock = std::chrono::steady_clock;
+using Timestamp = Clock::time_point;
+using Duration = Timestamp::duration;
+
+inline Timestamp Now() { return Clock::now(); }
+
+void UpdatePhysics(bool right,bool left, sf::RectangleShape& shape) {
+	if (right  == true) {
+		shape.move(0.1, 0);
+	}
+	if (left == true) {
+		shape.move(-0.1, 0);
+	}
+
+}
 
 int main() {
-	RenderWindow window(VideoMode(640, 480),"demo");
+	sf::RenderWindow window(sf::VideoMode(640, 480),"demo");
 	window.setFramerateLimit(120);
 	int x = 320;
 	int y = 280;
-	
+	sf::RectangleShape shape(sf::Vector2f(20, 20));
+	shape.setPosition(x, y);
+	shape.setFillColor(sf::Color::Blue);
+
+	const Duration kUpdatePeriod = std::chrono::milliseconds(10);
+	auto update_timestamp = Now();
 	while (window.isOpen()) {
+		bool right = false;
+		bool left = false;
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
-			if (event.KeyPressed) {
-				if (event.key.code == Keyboard::Right) {
-					++x;
-				}
-				if (event.key.code == Keyboard::Left) {
-					--x;
-				}
-				if (event.key.code == Keyboard::Escape) {
-					window.close();
-				}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+				right = true;
 			}
-
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+				left = true;
+			}
 		}
 
-		window.clear(Color::White);
+		if (Now() < update_timestamp) {
+			std::this_thread::sleep_until(update_timestamp);
+		}
 
-		Texture texture;
+		const auto current_time = Now();
+
+		
+		while (current_time >= update_timestamp) {
+			update_timestamp += kUpdatePeriod;
+			UpdatePhysics(right,left,shape);
+		}
+
+		window.clear(sf::Color::White);
+		
+		sf::Texture texture;
 		texture.loadFromFile("C:\\Users\\Alexandr\\Desktop\\Projects\\sfml-demo\\sfml-demo\\image.png");
 		texture.setSmooth(true);
-		Sprite sprite(texture, IntRect(0, 0, 640, 20));
+		sf::Sprite sprite(texture, sf::IntRect(0, 0, 640, 20));
 		sprite.setPosition(0,300);
 		window.draw(sprite);
 
-		RectangleShape shape(Vector2f(20,20));
-		shape.setFillColor(Color::Blue);
-		shape.setPosition(x, y);
 		window.draw(shape);
 		window.display();
 	}
