408f8c80c4456bf2dfa6678c075a7e44cbf2f6d4
Alexandr <cteblezov@gmail.com>
2021-11-25 23:11:21 +0700

    SneakGame

--- src/main.cpp
+++ src/main.cpp
@@ -12,75 +12,81 @@ using Duration = Timestamp::duration;
 inline Timestamp Now() { return Clock::now(); }
 
 
-std::pair<int, int> sneak_pos[200];
+sf::Vector2i sneak_pos[200];
 
-int dir ;
+int dir = 4;
 int score = 1;
 bool over = false;
+const int map_H = 20;
+const int map_W = 27;
+const int sizeS = 24;
 
-void UpdatePhysics() {
+void UpdatePhysics(sf::Sprite& sneak_head) {
 	for (int i = score; i > 0; --i) {
-		sneak_pos[i].first = sneak_pos[i - 1].first;
-		sneak_pos[i].second = sneak_pos[i - 1].second;
+		sneak_pos[i].x = sneak_pos[i - 1].x;
+		sneak_pos[i].y = sneak_pos[i - 1].y;
 	}
 
-	if (dir == 1) {
-		sneak_pos[0].first += 1;
+	switch (dir) {
+	case 1:
+		sneak_pos[0].x += 1;
+		sneak_head.setOrigin(12, 12);
+		sneak_head.setRotation(90);
+		sneak_head.setOrigin(0, 24);
+		break;
+	case  2:
+		sneak_pos[0].x -= 1;
+		sneak_head.setOrigin(12, 12);
+		sneak_head.setRotation(-90);
+		sneak_head.setOrigin(24, 0);
+		break;
+	case 3:
+		sneak_pos[0].y -= 1;
+		sneak_head.setOrigin(12, 12);
+		sneak_head.setRotation(360);
+		sneak_head.setOrigin(0, 0);
+		break;
+	case 4:
+		sneak_pos[0].y += 1;
+		sneak_head.setOrigin(12, 12);
+		sneak_head.setRotation(-180);
+		sneak_head.setOrigin(24, 24);
+		break;
 	}
-	if (dir == 2) {
-		sneak_pos[0].first -= 1;
-	}
-	if (dir == 3) {
-		sneak_pos[0].second-=1;
-	}
-	if (dir == 4) {
-		sneak_pos[0].second += 1;
-	}
-	if (sneak_pos[0].first > 27)
-		sneak_pos[0].first= 0; 
-	if (sneak_pos[0].first < 0) 
-		sneak_pos[0].first = 27;
-	if (sneak_pos[0].second > 20) 
-		sneak_pos[0].second = 0;  
-	if (sneak_pos[0].second < 0) 
-		sneak_pos[0].second = 20;
+	if (sneak_pos[0].x > map_W - 1)
+		sneak_pos[0].x= 0; 
+	if (sneak_pos[0].x < 0) 
+		sneak_pos[0].x = map_W - 1;
+	if (sneak_pos[0].y > map_H) 
+		sneak_pos[0].y = 0;  
+	if (sneak_pos[0].y < 0) 
+		sneak_pos[0].y = map_H;
 	for (int i = 1; i < score; i++) {
-		if (sneak_pos[0].first == sneak_pos[i].first && sneak_pos[0].second == sneak_pos[i].second)
+		if (sneak_pos[0].x == sneak_pos[i].x && sneak_pos[0].y == sneak_pos[i].y)
 			over = true;
 	}
 }
 
 
 int main() {
+	srand(time(0));
 	sf::RenderWindow window(sf::VideoMode(640, 480), "demo");
 
 	window.setFramerateLimit(60);
 
-	const int map_height = 20;
-	const int map_width = 27;
-
-
-	sf::Image map_image;
-	map_image.loadFromFile("map.png");
 	sf::Texture texture_map;
-	texture_map.loadFromImage(map_image);
+	texture_map.loadFromFile("map.png");
 	sf::Sprite map(texture_map);
 
-	sf::Image image_sneak_head;
-	image_sneak_head.loadFromFile("Sprite-0002.png");
-	sf::Image image_sneak_body;
-	image_sneak_body.loadFromFile("Sprite-0001.png");
 	sf::Texture texture_sneak_body;
-	texture_sneak_body.loadFromImage(image_sneak_body);
+	texture_sneak_body.loadFromFile ("Sprite-0001.png");
 	sf::Texture texture_sneak_head;
-	texture_sneak_head.loadFromImage(image_sneak_head);
+	texture_sneak_head.loadFromFile("Sprite-0002.png");
 	sf::Sprite sneak_head(texture_sneak_head);
 	sf::Sprite sneak_body(texture_sneak_body);
+	sneak_head.setRotation(-180);
 	
-	
-
-
-	int time_GameOver = 5;
+	int time_GameOver = 10;
 
 	sf::Text gameOver;
 	sf::Font font;
@@ -103,14 +109,11 @@ int main() {
 
 	int x_food;
 	int y_food;
-	srand(time(0));
-	x_food =  rand() % 27;
-	y_food =  rand() % 20;
+	x_food =  rand() % map_W;
+	y_food =  rand() % map_H;
 
-	sf::Image image_food;
-	image_food.loadFromFile("Sprite-0003.png");
 	sf::Texture texture_food;
-	texture_food.loadFromImage(image_food);
+	texture_food.loadFromFile("Sprite-0003.png");
 	sf::Sprite food(texture_food);
 	
 
@@ -137,37 +140,28 @@ int main() {
 				}
 				if (event.key.code == sf::Keyboard::Right) {
 					dir = 1;
-					sneak_head.setOrigin(12, 12);
-					sneak_head.setRotation(90);
-					sneak_head.setOrigin(0, 24);
+					
 				}
 				if (event.key.code == sf::Keyboard::Left) {
 					dir = 2;
-					sneak_head.setOrigin(12, 12);
-					sneak_head.setRotation(-90);
-					sneak_head.setOrigin(24, 0);
 				}
 				if (event.key.code == sf::Keyboard::Up) {
 					dir = 3;
-					sneak_head.setOrigin(12, 12);
-					sneak_head.setRotation(360);
-					sneak_head.setOrigin(0, 0);
+					
 				}
 				if (event.key.code == sf::Keyboard::Down) {
 					dir = 4;
-					sneak_head.setOrigin(12, 12);
-					sneak_head.setRotation(-180);
-					sneak_head.setOrigin(24, 24);
+					
 				}
 			}
 
 
 
 		}
-		if (sneak_pos[0].first == x_food && sneak_pos[0].second == y_food) {
+		if (sneak_pos[0].x == x_food && sneak_pos[0].y == y_food) {
 			++score;
-			x_food =  rand() % 27;
-			y_food =  rand() % 20;
+			x_food =  rand() % map_W;
+			y_food =  rand() % map_H;
 			
 
 		}
@@ -178,7 +172,8 @@ int main() {
 
 		if(timer > delay){
 			timer = 0;
-			UpdatePhysics();
+			if (over == false)
+				UpdatePhysics(sneak_head);
 			if (over == true) {
 				--time_GameOver;
 			}
@@ -188,10 +183,10 @@ int main() {
 
 		}
 		window.clear(sf::Color::White);
-		for (int i = 0; i < map_height; i++)
-			for (int j = 0; j < map_width; j++)
+		for (int i = 0; i < map_H; i++)
+			for (int j = 0; j < map_W; j++)
 			{
-				map.setPosition(j  * 24, i * 24 );
+				map.setPosition(j  * sizeS, i * sizeS );
 				window.draw(map);
 			}
 
@@ -200,20 +195,21 @@ int main() {
 		if (over == true) {
 			window.draw(gameOver);
 		}
-		window.draw(food);
-		window.draw(Score);
+		
 		for (int i = 0; i < score; ++i) {
 			if (i == 0) {
-				sneak_head.setPosition(sneak_pos[i].first * 24 , sneak_pos[i].second * 24 );
+				sneak_head.setPosition(sneak_pos[i].x* sizeS , sneak_pos[i].y * sizeS );
 				window.draw(sneak_head);
 			}
 			else {
-				sneak_body.setPosition(sneak_pos[i].first * 24, sneak_pos[i].second  * 24);
+				sneak_body.setPosition(sneak_pos[i].x * sizeS, sneak_pos[i].y * sizeS);
 				window.draw(sneak_body);
 			}
 			
 		}
 
+		window.draw(food);
+		window.draw(Score);
 		window.display();
 	}
 }
